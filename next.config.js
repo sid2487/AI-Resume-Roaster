11ed31b6b44bfa7696f8242772c7fbd5d4c7553c
sid2487 <sid454804@gmail.com>
2025-11-19 17:18:32 +0530

    dsjf d

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbo: false, // 🚀 FIX: disable Turbopack so file uploads work
-  },
-};
-
-module.exports = nextConfig;
